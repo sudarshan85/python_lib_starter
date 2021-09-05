c598384e8db0d289f6c99cca71a1a42992d42a0d
Sudarshan <su0@ornl.gov>
2021-09-05 10:30:03 -0400

    Entry point

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ import starter
 setup(
   name='starter',
   version=starter.__version__,
-  packages=find_packages(include=['starter', 'starter.*'])
+  packages=find_packages(include=['starter', 'starter.*']),
+  install_requires=open('./requirements.txt').readlines(),
+  entry_points={
+    'console_scripts': ['startit=starter.main:entry_point']
+  }
 )
--- starter/main.py
+++ starter/main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-def hello(name: str='Starter') -> str:
+import argparse
+
+def hello(name: str) -> str:
   """Test function that prints hello world
 
-  :param name: name to be displayed, defaults to 'Starter'
+  :param name: name to be displayed
   :type name: str, optional
   :return: hello world string
   :rtype: str
   """
   return f'Hello World! from {name}'
 
-if __name__=='__main__':
-  pass
+def parse_args():
+  parser = argparse.ArgumentParser(description="Console script to run starter", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-n', '--name', type=str, help='String to be displayed for hello world', default='Starter')
+  return parser.parse_args()
+
+def entry_point():
+  args = parse_args()
+  print(hello(args.name))
